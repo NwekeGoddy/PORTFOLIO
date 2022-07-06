d9d818e4cafd11270fe259809cb9b541875eb522
Nweke Chidi <nwekechidigodwin460@gmail.com>
2022-07-06 17:22:11 +0100

    Mobile nav fixed halfway
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ let bar2 = document.querySelector(".bar2");
 let bar3 = document.querySelector(".bar3");
 
 let asideMobile = document.querySelector(".aside-mobilenav");
+let mobilelink = document.querySelectorAll(".mobile-navbar-links li a")
 
 hamburger.addEventListener("click", function() {
   bar1.classList.toggle("change");
@@ -27,6 +28,12 @@ hamburger.addEventListener("click", function() {
   asideMobile.classList.toggle("displayToggle"); 
 });
 
-
+for(let i = 0 ; i < mobilelink.length ; i++){
+mobilelink[i].addEventListener("click", function() {
+  bar1.classList.remove("change");
+  bar2.classList.remove("change");
+  bar3.classList.remove("change");
+  asideMobile.classList.remove("displayToggle"); 
+});}
 
 
