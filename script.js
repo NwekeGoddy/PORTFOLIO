1ccdb9653c4e88360cfb2bde5d812951f92b8d6f
Nweke Chidi <nwekechidigodwin460@gmail.com>
2022-07-06 13:05:25 +0100

    Fun Moments Added
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ function scrollFunction() {
     document.getElementById("navbar").style.opacity = 1;
   }
 }
+
+
+let hamburger = document.querySelector(".hamburger");
+let bar1 = document.querySelector(".bar1");
+let bar2 = document.querySelector(".bar2");
+let bar3 = document.querySelector(".bar3");
+
+let asideMobile = document.querySelector(".aside-mobilenav");
+
+hamburger.addEventListener("click", function() {
+  bar1.classList.toggle("change");
+  bar2.classList.toggle("change");
+  bar3.classList.toggle("change");
+  asideMobile.classList.toggle("displayToggle"); 
+});
+
+
+
+
